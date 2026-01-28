f82cae414fefa936fd1e56dde8be5b0544b6b5f8
evan-duevel <evanjduevel@protonmail.com>
2026-01-27 18:04:42 -0600

    !!!

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello Universe");
-=======
         System.out.println("Hello, Universe!!!");
->>>>>>> new-features
     }
 }
